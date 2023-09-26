72845ec192cb0e84b350b91b63f24d0aae43e513
Barbara <barbaraelisabethb@gmail.com>
2023-09-26 16:28:11 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import inventoryLogo from "../static/inventoryLogo.jpg";
 import merchantLogo from "../static/merchantLogo.jpg"
+import inventoryLogo from "../static/inventoryLogo.jpg";
 import producerLogo from "../static/producerLogo.jpg"
-import { getBoxes} from '../services/BoxService';
-import BoxManager from './Box/BoxManager';
+import { getBoxes } from '../services/BoxService';
 import "../App.css";
 
-const Home = ( ) => {
+const Home = () => {
   const [boxes, setBoxes] = useState([]);
 
   useEffect(() => {
@@ -15,62 +14,44 @@ const Home = ( ) => {
     if (boxes.length) {
       return;
     }
-    getBoxes()
-      .then(data => {
-        if (mounted) {
-          setBoxes(data);
-        }
-      });
+    getBoxes().then((data) => {
+      if (mounted) {
+        setBoxes(data);
+      }
+    });
     return () => {
       mounted = false;
     };
   }, [boxes]);
 
+  const renderCard = (title, imgSrc, keyType, borrowedProperty) => (
+    <Card className="cards-style">
+      <Card.Img variant="top" src={imgSrc} />
+      <Card.Body className="cards">
+        <Card.Title>
+          <h4 className="cards-title">{title}</h4>
+        </Card.Title>
+        <Card.Text className="cards">
+          <span className="span-css">Total of premium boxes</span> <br />
+          {boxes
+            .filter((bx) => bx.box_type === 'premium')
+            .map((bx) => <p key={bx[keyType]}>{bx[borrowedProperty]}</p>)}
+          <span className="span-css">Total of common boxes</span> <br />
+          {boxes
+            .filter((bx) => bx.box_type === 'common')
+            .map((bx) => <p key={bx[keyType]}>{bx[borrowedProperty]}</p>)}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+
   return (
     <div className="cards-container">
-    <div className="d-flex justify-content-around">
-      <Card className='cards-style'>
-        <Card.Img variant="top" src={producerLogo} />
-        <Card.Body className='cards'>
-          <Card.Title><h4 className='cards-title'> Producers </h4></Card.Title>
-          <Card.Text className='cards'>
-            <span className='span-css'>Total of premium boxes</span> <br />
-            {boxes.map(bx => bx.box_type === 'premium' ? <p key={bx.box_type}>{bx.borrowed_producer}</p> : null)} 
-            <span className='span-css'>Total of common boxes</span> <br />
-            {boxes.map(bx => bx.box_type === 'common' ? <p key={bx.box_type}>{bx.borrowed_producer}</p> : null)} 
-          </Card.Text>
-        </Card.Body>
-      </Card>
-
-
-      <Card className='cards-style'>
-        <Card.Img variant="top" src={merchantLogo} />
-        <Card.Body className='cards'>
-          <Card.Title><h4 className='cards-title'> Merchants </h4></Card.Title>
-          <Card.Text className='cards'>
-            <span className='span-css'>Total of premium boxes</span> <br />
-            {boxes.map(bx => bx.box_type === 'premium' ? <p key={bx.box_type}>{bx.borrowed_merchant}</p> : null)}
-            <span className='span-css'>Total of common boxes</span> <br />
-            {boxes.map(bx => bx.box_type === 'common' ? <p key={bx.box_type}>{bx.borrowed_merchant}</p> : null)}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-
-
-      <Card className='cards-style'>
-        <Card.Img variant="top" src={inventoryLogo} />
-        <Card.Body className='cards'>
-          <Card.Title> <h4 className='cards-title'> Storehouse </h4></Card.Title>
-          <Card.Text className='cards'>
-          <span className='span-css'>Total of premium boxes</span> <br />
-          {boxes.map(bx => bx.box_type === 'premium' ? <p key={bx.box_type}>{bx.box_qtt}</p> : null)} 
-          <span className='span-css'>Total of common boxes</span> <br />
-          {boxes.map(bx => bx.box_type === 'common' ? <p key={bx.box_type}>{bx.box_qtt}</p> : null)} 
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
-
+      <div className="d-flex justify-content-around">
+        {renderCard('Producers', producerLogo, 'box_type', 'borrowed_producer')}
+        {renderCard('Merchants', merchantLogo, 'box_type', 'borrowed_merchant')}
+        {renderCard('Storehouse', inventoryLogo, 'box_type', 'box_qtt')}
+      </div>
     </div>
   );
 }
