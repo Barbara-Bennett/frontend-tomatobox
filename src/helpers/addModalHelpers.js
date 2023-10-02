f2d5017b2072ebbd0ff4575b6099be2c550d132e
Barbara <barbaraelisabethb@gmail.com>
2023-10-02 10:55:56 -0400

    Add addModalHelpers.js

--- src/helpers/addModalHelpers.js
+++ src/helpers/addModalHelpers.js
@@ -1,13 +1,23 @@
 import React from "react";
-import AddProducerModal from "../components/Producer/AddProducerModal";
+import AddNewBoxModal from "../components/Box/AddNewBoxModal";
+import AddMerchantModal from "../components/Merchant/AddMerchantModal";
 import AddMerchantTransactionModal from "../components/MerchantTransaction/AddMerchantTransactionModal";
+import AddProducerModal from "../components/Producer/AddProducerModal";
+import AddProducerTransactionModal from "../components/ProducerTransactionModal/AddProducerTransactionModal";
 
 function getModalByType(modalType, props) {
   switch (modalType) {
+    case "addBox":
+      return <AddNewBoxModal {...props} />;
+    case "addMerchantTransaction":
+      return <AddMerchantTransactionModal {...props} />;
+    case "addMerchant":
+      return <AddMerchantModal {...props} />;
     case "addProducer":
       return <AddProducerModal {...props} />;
-    case "addMerchantTransaction":
-        return <AddMerchantTransactionModal {... props} />
+    case "addProducerTransaction":
+      return <AddProducerTransactionModal {...props} />;
+
     default:
       return null;
   }
